04d51fca867907f2f9ffa0a3295722cde63b9924
Kennedy Senyo Dordoe <kennedysenyo@gmail.com>
2025-01-02 18:47:38 +0000

    change array of numbes to array of objects

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,25 @@ import Die from "./components/Die"
 
 const App = () => { 
 
+  const [isHeld, setIsHeld] = useState(false);
   const [dice, setDice] = useState(generateAllNewDice())
 
   function generateAllNewDice() {
     return new Array(10)
       .fill(0)
-      .map(() => Math.ceil(Math.random() * 6))
+      .map((dice, index) => ({
+        id: index+1, 
+        value: Math.ceil(Math.random() * 6), 
+        isHeld:  isHeld
+      }))
   }
 
   function rollDice() {
     setDice(generateAllNewDice())
+    console.log(dice)
   }
 
-  const diceElements = dice.map((die) =>  <Die value={die}/>)
- 
-
+  const diceElements = dice.map((die) =>  <Die key={die.id} value={die.value}/>)
 
   return (
     <main>

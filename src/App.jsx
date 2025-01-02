32ab8caa92c2d270f97666a737c84c379f8151e7
Kennedy Senyo Dordoe <kennedysenyo@gmail.com>
2025-01-02 18:20:03 +0000

    render Die components by mapping to array

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
+import { useState } from "react"
 import Die from "./components/Die"
 
 const App = () => { 
 
+  const [dice, setDice] = useState(generateAllNewDice())
+
+  function generateAllNewDice() {
+    return new Array(10)
+      .fill(0)
+      .map(() => Math.ceil(Math.random() * 6))
+  }
+
+  function rollDice() {
+    setDice(generateAllNewDice())
+  }
+
+  const diceElements = dice.map((die) =>  <Die value={die}/>)
+ 
+
+
   return (
     <main>
       <div className="dice-container">
-      <Die value={1}/>
-      <Die value={2}/>
-      <Die value={3}/>
-      <Die value={4}/>
-      <Die value={5}/>
-      <Die value={6}/>
-      <Die value={7}/>
-      <Die value={8}/>
-      <Die value={9}/>
-      <Die value={10}/>
+        { diceElements }      
       </div>
+      <button 
+        className="roll-dice"
+        onClick={rollDice}
+      >Roll</button>
     </main>
   )
 }

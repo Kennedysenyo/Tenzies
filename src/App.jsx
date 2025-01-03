28be03a166aa2cf4df16c821e59ccab269c35f03
Kennedy Senyo Dordoe <kennedysenyo@gmail.com>
2025-01-03 12:55:28 +0000

    Added some accessibility improvements

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
-import { useState } from "react"
+import { useState, useRef, useEffect } from "react"
 import Die from "./components/Die"
 import { useWindowSize } from "react-use"
 import Confetti from "react-confetti"
 
 const App = () => { 
 
-  const [dice, setDice] = useState(generateAllNewDice())
+  const [dice, setDice] = useState(() => generateAllNewDice());
+  const buttonRef = useRef(null);
   
   const gameWon = (
     dice.every(die => die.isHeld) && 
     dice.every(die => die.value === dice[0].value)
   )  
 
+  useEffect(() => {
+    if (gameWon) {
+      buttonRef.current.focus();
+    }
+  }, [gameWon])
+
   function generateAllNewDice() {
     return new Array(10)
       .fill(0)
-      .map((dice, index) => ({
+      .map((_, index) => ({
         id: index+1, 
         value: Math.ceil(Math.random() * 6), 
         isHeld:  false
@@ -73,6 +80,9 @@ const App = () => {
   return (
     <main>
       { gameWon && <Confetti width={width} height={height} /> }
+      <div aria-live="polite" className="sr-only">
+        {gameWon && <p>Congratulations! You won! Press "New Game" to start again.</p>}
+      </div>
       <h1 className="title">Tenzies</h1>
       <p className="instructions">
         Roll until all dice are the same. Click each die to freeze it at its current value between rolls.
@@ -80,7 +90,7 @@ const App = () => {
       <div className="dice-container">
         { diceElements }      
       </div>
-      <button className="roll-dice" onClick={rollDice}>
+      <button ref={buttonRef} className="roll-dice" onClick={rollDice}>
         { gameWon ? "New Game" : "Roll" }
       </button>
     </main>
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -4,7 +4,10 @@ const Die = (props) => {
   return (
     <button 
       onClick={() => props.hold(props.id)} 
-      style={{backgroundColor: props.held ? "#59E391" : "white"}}    
+      style={{backgroundColor: props.held ? "#59E391" : "white"}} 
+      aria-pressed={props.held}  
+      aria-label={`Die with a value of ${props.value}, 
+      ${props.held ? "held" : "not held"}`} 
     >{props.value}</button>
   )
 }
